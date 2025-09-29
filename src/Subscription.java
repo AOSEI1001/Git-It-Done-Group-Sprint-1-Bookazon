ab86af98fb410ccf9f06c366ec0551a3436ebfac
Tony <ooonab26@colby.edu>
2025-09-29 16:06:30 -0400

    Added Subscription class

---
+++ src/Subscription.java
@@ -0,0 +1,5 @@
+public class Subscription {
+    private final String sub_type;
+    public Subscription(String sub_type) { this.sub_type = sub_type; }
+    public String getSubscription() { return sub_type; }
+}
