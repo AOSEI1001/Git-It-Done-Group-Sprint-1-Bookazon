10b701f20d641e13105de222b14de57915ffa057
AOSEI1001 <abisaamankwah51@yahoo.com>
2025-09-29 11:52:14 -0400

    Delete duplicate code in User and Order, add address, zipCode, and shippingInfo classes

---
+++ src/Address.java
@@ -0,0 +1,24 @@
+public class Address {
+
+    private String line1;
+    private String line2;
+    private String city;
+    private String state;
+    private ZipCode zip;
+    private Country country;
+
+    public Address(String line1, String line2, String city, String state, ZipCode zip, Country country){
+        this.line1 = line1;
+        this.line2 = line2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+
+    }
+    
+    
+}
+
+
+
