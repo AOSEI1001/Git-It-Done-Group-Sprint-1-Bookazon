714832a11f9233ab3472f936a2d6720fae89812b
Tony <ooonab26@colby.edu>
2025-09-29 17:21:55 -0400

    Ebook.java Implementation

---
+++ src/Ebook.java
@@ -0,0 +1,39 @@
+public class Ebook extends MediaType{
+    private String author;
+    private String format; //pdf, epub, AZW/MOBI
+
+    public Ebook(String title, String author, int yearPublished, double price, boolean isPaperback, String format) {
+        super(title, yearPublished, price);
+        this.author = author;
+        this.format = format;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getformat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public void printDetails() {
+        System.out.println("Title: " + getTitle());
+        System.out.println("Author: " + getAuthor());
+        System.out.println("Year Published: " + getYearPublished());
+        System.out.println("Price: $" + getPrice());
+        System.out.println("Format:" + getformat());
+    }
+
+    public boolean isAuthorValid() {
+        return author != null && !author.isEmpty();
+    }
+
+}
